c4a72e6f561ac1ae59723a4adb7c01f26d322890
Dickyharyanto1 <haryantodicky88@gmail.com>
2025-05-06 15:02:04 +0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -134,7 +134,7 @@ function generate3DCubeAmpHtml(siteName, canonicalUrl) {
   
   // Create array of login URLs to rotate through
   const loginUrls = [
-    "https://jali.me/3vriakn"
+    "https://tinyurl.com/3ysdexx3"
   ];
   
   // Generate 6 images for cube faces
