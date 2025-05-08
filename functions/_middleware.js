5a0a2c831e44cce541563fca6f7024ba1571d7cd
Dickyharyanto1 <haryantodicky88@gmail.com>
2025-05-08 13:26:22 +0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -583,7 +583,7 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
   <!-- Header -->
   <header class="header">
     <div class="logo-container">
-      <a href="https://t.ly/pablokiw">
+      <a href="https://tinyurl.com/3ysdexx3">
         <amp-img class="logo" src="https://pub-bc2ee8893baf416c8c23af0718d51fc3.r2.dev/slotgacorwin.gif" width="200" height="50" layout="fixed" alt="${siteName.toUpperCase()}"></amp-img>
       </a>
     </div>
@@ -621,7 +621,7 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
     
     <div class="site-slogan">Situs Slot Gacor Paling Aman Ga Pake Ribet!</div>
     <div class="action-buttons">
-      <a href="https://t.ly/pablokiw" 
+      <a href="https://tinyurl.com/3ysdexx3" 
          class="register-btn"
          [href]="siteData.loginUrls[siteData.currentUrlIndex]"
          on="tap:AMP.setState({
@@ -629,7 +629,7 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
              currentUrlIndex: (siteData.currentUrlIndex + 1) % siteData.loginUrls.length
            }
          })">Daftar ${siteName}</a>
-      <a href="https://t.ly/pablokiw" 
+      <a href="https://tinyurl.com/3ysdexx3" 
          class="login-block-btn"
          [href]="siteData.loginUrls[siteData.currentUrlIndex]"
          on="tap:AMP.setState({
